1978fa143ceec8785d68036d2ce37b4da161eb09
Bilalkhan90 <46218938+Bilalkhan90@users.noreply.github.com>
2018-12-28 15:21:26 +0500

    Add files via upload
    table in loop

---
+++ table.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+ using namespace std;
+ main()
+ {
+ 	int a,b,result,i;
+ 	b=1;
+ 	cout<<"enter num";
+ 	cin>>a;
+ 	cout<<"Table of"<<a<<endl;
+ 	for(i=1;i<a;i++)
+ 	
+	 
+ 	while(b<=10)
+ 	{
+ 		
+ 		result=b*a;
+ 		cout<<a<<"*"<<b<<"="<<result<<endl;
+ 		b++;
+	 }
+
+ }
